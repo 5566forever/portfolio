aa274dffe254699ee5e368dbfeb6af620d272f3e
Liang <>
2022-02-24 19:35:51 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#121212',
+        'selected-text': '#A3A3FF',
+        'theme': '#B53061',
+        'light':'#D2145A',
+        'nav': '#404053',
+        'secondary': '#9191A4',
+        'badge': '#3F3F51',
+        'input-border': '#565666',
+        'input': '#2A2A35'
+      },
+      fontFamily: {
+        'Noto Serif TC': ['Noto Serif TC', 'serif'],
+        'Roboto': ['Roboto', 'sans-serif'],      
+      }
+    },
+  }
+}
